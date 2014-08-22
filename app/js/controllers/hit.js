1c05c34514e9bd40f3b079bdff32e9d835475893
CsabaBarsony <csaba.barsony@gmail.com>
2014-08-22 10:05:06 +0200

    update and sync works

--- app/js/controllers/hit.js
+++ app/js/controllers/hit.js
@@ -2,12 +2,16 @@
 
 app.controller("HitController", function($scope, FIRE_URL, $firebase){
 	var ref = new Firebase(FIRE_URL + "/hits");
-	var hits = $firebase(ref).$asArray();
-	$scope.hits = hits;
+	$scope.hits = $firebase(ref).$asArray();
+	$scope.loadingHits = false;
 
 	$scope.hitClick = function(id){
-		var hit = prompt("Enter hit!");
-		console.log(hit);
+		var hit = $scope.hits.$getRecord(id);
+		hit.name = prompt("Enter name!");
+		$scope.loadingHits = true;
+		$scope.hits.$save(hit).then(function(ref){
+			if(ref.name()) $scope.loadingHits = false;
+		});
 	};
 });
 
