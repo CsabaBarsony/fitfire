1701110be93b3d7aba68e015c0611951ac8a9b3b
CsabaBarsony <csaba.barsony@gmail.com>
2014-08-22 13:33:09 +0200

    startAt() not working...

--- app/js/controllers/hit.js
+++ app/js/controllers/hit.js
@@ -2,15 +2,47 @@
 
 app.controller("HitController", function($scope, FIRE_URL, $firebase){
 	var ref = new Firebase(FIRE_URL + "/hits");
-	$scope.hits = $firebase(ref).$asArray();
-	$scope.loadingHits = false;
+
+	$scope.position = 1;
+
+	$scope.back = function(){
+		if($scope.position > 1) $scope.position--;
+	};
+
+	$scope.forth = function(){
+		if($scope.position < 3) $scope.position++;
+	};
+
+	$scope.hitList = {
+		hit1: {},
+		hit2: {},
+		hit3: {}
+	};
+
+	$scope.hits = $firebase(ref.startAt("date", "2014-08-10").endAt("date", "2014-08-20")).$asObject()
+	$scope.sm = StateMachine.create({
+		initial: "idle",
+		events: [
+			{ name: "hitSaved", from: "loading", to: "idle" },
+			{ name: "hitSaving", from: "idle", to: "loading" }
+		],
+		callbacks: {
+			onidle: function() { $scope.loadingHits = false; },
+			onloading: function() { $scope.loadingHits = true }
+		}
+	});
+	$scope.hits.$watch(function(e){
+		console.log(e.event);
+	});
 
 	$scope.hitClick = function(id){
-		var hit = $scope.hits.$getRecord(id);
-		hit.name = prompt("Enter name!");
-		$scope.loadingHits = true;
-		$scope.hits.$save(hit).then(function(ref){
-			if(ref.name()) $scope.loadingHits = false;
+		var x = 0;
+		return;
+		var mod = $scope.hits.$getRecord(id);
+		mod.name = prompt("Enter name!");
+		$scope.sm.hitSaving();
+		$scope.hits.$save(mod).then(function(ref){
+			if(ref.name()) $scope.sm.hitSaved();
 		});
 	};
 });
